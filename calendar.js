95497d971f2be7e041de7fcceb53e1b29e383e6e
Joe Longstreet <joelongstreet@gmail.com>
2014-06-10 12:58:41 -0500

    collect first event in each calendar which is either right now or in the future

--- calendar.js
+++ calendar.js
@@ -1,16 +1,61 @@
+var Q = require('q');
+var moment = require('moment-timezone');
 var GoogleCalendar = require('google-calendar');
 
-exports.getCurrentEvent = function(accessToken){
-  var gcal = new GoogleCalendar.GoogleCalendar(accessToken);
-  gcal.calendarList.list(function(err, calendarList){
-    console.log(calendarList);
 
-    calendarList.forEach(function(calendar){
-      console.log(calendar);
+var getAllCalendars = function(gcal){
+  var deferred = Q.defer();
 
-      gcal.events.list(calendar.id, function(err, events){
-        console.log(events);
+  gcal.calendarList.list(function(err, calendars){
+    if(err) console.log(err);
+
+    if(calendars.items){
+      deferred.resolve({
+        gcal: gcal,
+        calendars: calendars.items
       });
-    });
-  })
+    }
+  });
+
+  return deferred.promise;
+};
+
+
+var getEventsFromCalendars = function(opts){
+  var deferred = Q.defer();
+  var completed = 0;
+  var allEvents = {};
+
+  opts.calendars.forEach(function(calendar){
+    var now = moment().tz(calendar.timeZone).format();
+
+    opts.gcal.events.list(
+      calendar.id,
+      { timeMin: now, maxResults: 1},
+      function(err, events){
+        allEvents[calendar.id] = (events.items);
+        completed++;
+        if(completed == opts.calendars.length){
+          deferred.resolve(allEvents);
+        }
+      }
+    );
+  });
+
+  return deferred.promise;
+};
+
+
+exports.getAllEvents = function(opts){
+  var deferred = Q.defer();
+  var gcal = new GoogleCalendar.GoogleCalendar(opts.accessToken);
+
+  getAllCalendars(gcal)
+    .then(getEventsFromCalendars)
+    .then(function(allEvents){
+      deferred.resolve(allEvents);
+    }
+  );
+
+  return deferred.promise;
 };
