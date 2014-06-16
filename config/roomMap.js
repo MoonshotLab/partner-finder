fd4b70f64f0a63f8abcabe0cd59c7f05237007f2
Joe Longstreet <joelongstreet@gmail.com>
2014-06-16 14:16:17 -0500

    map rooms to floors

---
+++ config/roomMap.js
@@ -0,0 +1,33 @@
+exports.floors = [{
+  1: [
+    '(KC) Room - Apollo',
+    '(KC) Room - Big Dipper',
+    '(KC) Room - Galaxy',
+    '(KC) Room - Haley\'s Comet',
+    '(KC) Room - Hubble',
+    '(KC) Room - Little Dipper',
+    '(KC) Room - North Star',
+    '(KC) Room - Orion',
+    ],
+  2: [
+    '(KC) Room - Buzz Aldrin',
+    '(KC) Room - Gus Grissom',
+    '(KC) Room - Innovation Lab',
+    '(KC) Room - Neil Armstrong',
+    '(KC) Room - Sally Ride',
+  ],
+  3: [
+    '(KC) Room - Earth',
+    '(KC) Room - Jupiter',
+    '(KC) Room - Mars',
+    '(KC) Room - Mercury',
+    '(KC) Room - Pluto',
+    '(KC) Room - Saturn',
+    '(KC) Room - Social Command Center',
+    '(KC) Room - Uranus',
+    '(KC) Room - Venus',
+  ],
+  4: [
+    '(KC) Room - CPG Room',
+  ]
+}];
