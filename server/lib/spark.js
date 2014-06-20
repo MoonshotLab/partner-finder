9b8c2584812203bbfd164d040ae5355f449be134
Joe Longstreet <joelongstreet@gmail.com>
2014-06-20 16:01:51 -0500

    ...make it work

--- server/lib/spark.js
+++ server/lib/spark.js
@@ -1,7 +1,9 @@
 var spark = require('sparknode');
 var userMap = require('../config/userMap');
 var roomMap = require('../config/roomMap');
+var calendar = require('./calendar');
 var db = require('./db');
+var auth = require('./auth');
 
 
 var findFloor = function(calendarEvent){
@@ -31,7 +33,16 @@ exports.init = function(){
     {}
   );
 
-  core.on('find-user', function(data){
+  core.on('error', function(e){
+    console.log('error connecting', e);
+  });
+
+  core.on('connect', function(e){
+    console.log('connected', e);
+  });
+
+  core.on('find-user', function(e){
+    var data = JSON.parse(e.data);
     var selectedUser = null;
     var sparkUserId = 0;
 
@@ -47,6 +58,8 @@ exports.init = function(){
         .then(auth.getNewAccessToken)
         .then(calendar.getNextEvent)
         .then(function(calendarEvent){
+          console.log('calendar event:', calendarEvent);
+
           // Pass params to spark n the form of {userId,floorNumber}
           var params = sparkUserId + ',' + findFloor(calendarEvent);
           core.notify(params, function(err, data){
