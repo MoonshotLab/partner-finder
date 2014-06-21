51eee968dcd617e17cadc28049c6330b40fd05c5
Joe Longstreet <joelongstreet@gmail.com>
2014-06-21 10:30:13 -0500

    handle errors on promises for the spark

--- server/lib/spark.js
+++ server/lib/spark.js
@@ -4,15 +4,18 @@ var calendar = require('./calendar');
 var db = require('./db');
 var auth = require('./auth');
 var utils = require('./utils');
+var core = null;
 
 
 var handleError = function(err){
   console.log('Spark Error', err, '\n');
 };
 
+
 var handleConnection = function(e){
   console.log('Spark Connected', e, '\n');
 };
+
 
 var handleEvent = function(e){
   var data = JSON.parse(e.data);
@@ -26,27 +29,35 @@ var handleEvent = function(e){
     }
   });
 
+  var promiseError = function(err){
+    console.log('Error handling promise:', err);
+    core.notify('0,?', function(err, data){
+      if(err) console.log('error notifying spark', err);
+    });
+  };
+
+  var notifySpark = function(calendarEvent){
+    utils.attachFloorToEvent(calendarEvent);
+    console.log('attempting to notify spark of event:', calendarEvent, '\n\n');
+
+    // Pass params to spark n the form of {userId,floorNumber}
+    var params = sparkUserId + ',' + calendarEvent.floor;
+    core.notify(params, function(err, data){
+      if(err) console.log('error notifying spark', err);
+    });
+  };
+
   if(selectedUser){
     db.findUser(selectedUser)
       .then(auth.getNewAccessToken)
       .then(calendar.getNextEvent)
-      .then(function(calendarEvent){
-
-        utils.attachFloorToEvent(calendarEvent);
-        console.log('attempting to notify spark of event:', calendarEvent, '\n\n');
-
-        // Pass params to spark n the form of {userId,floorNumber}
-        var params = sparkUserId + ',' + calendarEvent.floor;
-        core.notify(params, function(err, data){
-          if(err) console.log('error notifying spark', err);
-        });
-      }
-    );
+      .then(notifySpark)
+      .catch(promiseError);
   }
 };
 
 var init = function(){
-  var core = new spark.Core(
+  core = new spark.Core(
     process.env.SPARK_ACCESS_TOKEN,
     process.env.SPARK_CORE_ID,
     {}
